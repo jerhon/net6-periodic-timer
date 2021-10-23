88d256090df9457456d745524e9645055e27f2a5
Jeremy Honl <jerhon@users.noreply.github.com>
2021-10-22 20:11:04 -0500

    Add sample for new .NET 6 PeriodicTimer class.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// This is nothing special, just trying out the Periodic Timer in .NET 6
+
+using System.Diagnostics;
+
+using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+Stopwatch sw = Stopwatch.StartNew();
+while (await timer.WaitForNextTickAsync())
+{
+    Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds}ms");
+    await Task.Delay(500);
+}
